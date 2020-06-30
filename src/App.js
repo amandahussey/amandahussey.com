6da14eaeb84a8e4240890a034a67175ff153377d
Amanda Hussey <amanda.k.hussey@gmail.com>
2020-06-29 22:08:06 -0600

    first commit - color toggle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      background: 'light'
+    }
+  }
+
+  toggleColor = () => {
+    if(this.state.background === 'light'){
+      console.log('light')
+      this.setState({
+        background: 'dark'
+      })
+    }
+    else if (this.state.background === 'dark'){
+      console.log('dark')
+      this.setState({
+        background: 'light'
+      })
+    }
+  }
+
+  render (){
+    return (
+      <div className={ this.state.background === 'light' ? 'App--light' : 'App--dark' }>
+        <div className={ this.state.background === 'light' ? 'color-toggle--dark' : 'color-toggle--light' } onClick={this.toggleColor}></div>
+        <header className="App-header">
+          Amanda Hussey
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
