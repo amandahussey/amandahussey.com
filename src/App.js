b324052fa8be98077133c2626cd376bbbbfbdda8
Amanda Hussey <amanda.k.hussey@gmail.com>
2020-09-25 12:27:37 -0600

    SwingingChain begin

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SwingingChain from './SwingingChain'
+
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      background: 'light'
+      lightIsOn: false
     }
   }
 
   toggleColor = () => {
-    if(this.state.background === 'light'){
-      console.log('light')
-      this.setState({
-        background: 'dark'
-      })
-    }
-    else if (this.state.background === 'dark'){
-      console.log('dark')
-      this.setState({
-        background: 'light'
-      })
-    }
+    console.log('this.state.lightIsOn', this.state.lightIsOn)
+    this.setState({
+      lightIsOn: !this.state.lightIsOn
+    })
   }
 
   render (){
     return (
-      <div className={ this.state.background === 'light' ? 'App--light' : 'App--dark' }>
-        <div className={ this.state.background === 'light' ? 'color-toggle--dark' : 'color-toggle--light' } onClick={this.toggleColor}></div>
-        <header className="App-header">
+      <div className={ `App ${this.state.lightIsOn === true ? 'App--light' : 'App--dark' }` }>
+        <div className={ this.state.lightIsOn === true ? 'color-toggle dark-background' : 'color-toggle light-background' } onClick={this.toggleColor}></div>
+        <header className={`App-header ${this.state.lightIsOn === false ? 'glow-in-the-dark' : ''}` }>
           Amanda Hussey
         </header>
+        <SwingingChain/>
       </div>
     );
   }
---
+++ src/SwingingChain.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './SwingingChain.css';
+
+class SwingingChain extends Component {
+  constructor(){
+    super()
+    this.state = {
+      
+    }
+  }
+
+
+  render (){
+    return (
+        <div class="chain-container">
+
+        <div class="chandelier-holder">
+      
+          <div class="chain first-chains very-first-chain">
+            <div class="chain first-chains">
+              <div class="chain first-chains">
+                <div class="chain first-chains">
+                  <div class="chain first-chains">
+                    <div class="chain first-chains">
+                      <div class="chain first-chains">
+                        <div class="chain first-chains">
+                          <div class="chain first-chains">
+                            <div class="chain first-chains">
+                              <div class="chain first-chains">
+                                <div class="chain first-chains">
+                                  <div class="chain first-chains">
+                                    <div class="chain first-chains">
+                                      <div class="chain">
+                                        <div class="chain">
+                                          <div class="chain">
+                                            <div class="chain">
+                                              <div class="chain">
+                                                <div class="chain">
+                                                  <div class="chain">
+                                                    <div class="chain">
+                                                      <div class="chain">
+                                                        <div class="chain">
+                                                          <div class="chain">
+                                                            <div class="chain">
+                                                              <div class="chain">
+                                                                <div class="chain">
+                                                                  <div class="chain">
+                                                                    <div class="chain">
+                                                                      <div class="chain">
+                                                                        <div class="chain chandelier"></div>
+                                                                      </div>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                              </div>
+                                                            </div>
+                                                          </div>
+                                                        </div>
+                                                      </div>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default SwingingChain;
