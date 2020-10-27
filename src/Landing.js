26e96226acc289226bab257b4f6c9b578319251b
Amanda Hussey <amanda.k.hussey@gmail.com>
2020-10-27 00:27:04 -0600

    further work on generalizing step scroll animations

--- src/Landing.js
+++ src/Landing.js
@@ -24,22 +24,22 @@ class Landing extends Component {
             document.body.style.setProperty('--scroll', bottomScrollModPercentage);
 
             // console.log('topScroll', topScroll)
-            // console.log('bottomScroll', bottomScroll)
-            // console.log('windowHeight', windowHeight)
+            console.log('bottomScroll', bottomScroll)
+            console.log('windowHeight', windowHeight)
             // console.log('bottomScrollMod', bottomScrollMod)
             // console.log('documentHeight', documentHeight)
 
-            // const beginAnimation1 = windowHeight + (.37 * windowHeight) //  1467
-            // const endAnimation1 = windowHeight + (.66 * windowHeight)   // 2867
-            const beginAnimation1 = 1467
-            const endAnimation1 = 2867
+            const beginAnimation1 = windowHeight + (.37 * windowHeight) //  1467
+            const endAnimation1 = (2 * windowHeight) + (.66 * windowHeight)   // 2867
+            // const beginAnimation1 = 1467
+            // const endAnimation1 = 2867
             const percent1 = (bottomScroll - beginAnimation1) / (endAnimation1 - beginAnimation1);
             document.body.style.setProperty('--scroll-1', percent1);
 
-            // const beginAnimation2 = (4 * windowHeight) + (.95 * windowHeight) // 5275
-            // const endAnimation2 = documentHeight // 6075
-            const beginAnimation2 = 5275
-            const endAnimation2 = 6075
+            const beginAnimation2 = (4 * windowHeight) + (.95 * windowHeight) // 5275
+            const endAnimation2 = documentHeight // 6075
+            // const beginAnimation2 = 5275
+            // const endAnimation2 = 6075
             const percent2 = (bottomScroll - beginAnimation2) / (endAnimation2 - beginAnimation2);
             document.body.style.setProperty('--scroll-2', percent2);
             
