cc4c3b06b7f8fcbc7536d65b7db4750cce1f5d64
Amanda Hussey <amanda.k.hussey@gmail.com>
2020-12-14 12:44:17 -0700

    rework scroll animation for all devices + fix bug where 'Software Developer' showing on refresh

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -16,8 +16,8 @@ class Landing extends Component {
         return (
             <div id='landing__container'>
                 <div id='landing__text-container'>
-                    <h1 id='landing__text-container__name' className={ navIsFixed ? 'fixed-name' : ''}>Amanda Hussey</h1>
-                    <h2 id='landing__text-container__software-dev'>Software Developer</h2>
+                    <h1 id='landing__text-container__name' className={ navIsFixed ? 'fix-name' : ''}>Amanda Hussey</h1>
+                    <h2 id='landing__text-container__software-dev' className={ navIsFixed ? 'hide-title' : ''}>Software Developer</h2>
                 </div>
             </div>
             
