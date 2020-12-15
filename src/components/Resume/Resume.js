b092fddef7955c782671390994da64c70c4a946e
Daniel Gopen <daniel.gopen@gmail.com>
2020-12-14 21:59:33 -0700

    style adjustments

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,39 +5,38 @@ class Resume extends Component {
     constructor(){
         super()
         this.state = {
-        
+
         }
     }
 
-  
+
 
   render (){
-   
+
         return (
             <div>
                 <div id='software-development' className='resume-parent-container'>
                 <div className='resume-container'>
                     <h1 className='resume-section__title'>Software Development</h1>
-                    
+
                     <div className='resume-section'>
                         <div className='resume-section__header'>
                             <h1>Zayo Group</h1>
                             <h2>May 2020 - Sept 2020</h2>
                         </div>
 
-                        <h3>Application Developer</h3>
+                        <h3 className='resume-section__job-title'>Application Developer</h3>
                         <p className='resume-section__technologies-used'>
                             <span>[ React + Redux ] </span>
                             <span>[ AngularJS ] </span>
                             <span>[ NodeJS ]</span>
                         </p>
-                        <div className='resume-section__description'> 
+                        <div className='resume-section__description'>
                             <div className='resume-section__description__greatest-moment'>Greatest Contribution:</div>
-                            <div className='resume-section__description__paragraph'>   
+                            <div className='resume-section__description__paragraph'>
                                 <p>Worked closely with designer to develop strategies around delivering best UI/UX practices.</p>
                                 <p>Onboarded junior hire. I truly enjoyed acting as a teacher/mentor!</p>
                             </div>
-                            {/* <p className='resume-section__description__takeaway'>I value UI/UX.</p> */}
                         </div>
                     </div>
 
@@ -47,7 +46,7 @@ class Resume extends Component {
                             <h2>Sept 2019 - May 2020</h2>
                         </div>
 
-                        <h3>Front End Developer</h3>
+                        <h3 className='resume-section__job-title'>Front End Developer</h3>
                         <p className='resume-section__technologies-used'>
                             <span>[ React + Redux ] </span>
                             <span>[ jQuery + ES6 template literals ]</span>
@@ -55,11 +54,10 @@ class Resume extends Component {
 
                         <div className='resume-section__description'>
                             <div className='resume-section__description__greatest-moment'>Proudest Moment:</div>
-                            <div className='resume-section__description__paragraph'>   
-                                <p>Led 10+ project scoping meetings, which received positive feedback through to executive level.</p> 
-                                <p>I enjoyed being a go-between of sorts, helping bridge the gap between techincal and non-technical.</p> 
+                            <div className='resume-section__description__paragraph'>
+                                <p>Led 10+ project scoping meetings, which received positive feedback through to executive level.</p>
+                                <p>I enjoyed being a go-between of sorts, helping bridge the gap between techincal and non-technical.</p>
                             </div>
-                            {/* <p className='resume-section__description__takeaway'>I bridge the gap between technical and non-technical.</p> */}
                         </div>
                     </div>
 
@@ -69,23 +67,21 @@ class Resume extends Component {
                             <h2>July 2018 - April 2019</h2>
                         </div>
 
-                        <h3>Junior Software Developer</h3>
+                        <h3 className='resume-section__job-title'>Junior Software Developer</h3>
                         <p className='resume-section__technologies-used'>
                             <span>[ AngularJS ] </span>
                             <span>[ SQL (SSMS) ]</span>
                         </p>
-                        
+
 
                         <div className='resume-section__description'>
                             <div className='resume-section__description__greatest-moment'>Greatest Accomplishment:</div>
-                            <div className='resume-section__description__paragraph'>   
+                            <div className='resume-section__description__paragraph'>
                                 <p>The Pack Station - A key feature of the web app. UI designed by me!</p>
                                 <p>Executives loved it so much, that they made sure it was shown in all major sales demos. It was even planned to be used in an advertising campaign.</p>
                             </div>
-
-                            {/* <p className='resume-section__description__takeaway'>I offer alternative solutions.</p> */}
-
                         </div>
+
                     </div>
                 </div>
                 </div>
@@ -98,28 +94,28 @@ class Resume extends Component {
                             </div>
                             <div className='resume-section__description'>
                                 <div className='resume-section__sub-title'>
-                                    <h3>Graduate Coursework</h3>
+                                    <h3 className='resume-section__education-title'>Graduate Coursework</h3>
                                     <h3>Aug 2014 - Oct 2015</h3>
                                 </div>
                                 <p>MS Student in Pure Mathematics</p>
                             </div>
                             <div className='resume-section__description'>
                                 <div className='resume-section__sub-title'>
-                                    <h3>Teaching Assistant</h3>
+                                    <h3 className='resume-section__education-title'>Teaching Assistant</h3>
                                     <h3>Aug 2014 - Oct 2015</h3>
                                 </div>
                                 <p>Awarded Honorable Mention for Outstanding Teaching</p>
                             </div>
                             <div className='resume-section__description'>
                                 <div className='resume-section__sub-title'>
-                                    <h3>Bachelor of Science in Mathematics</h3>
+                                    <h3 className='resume-section__education-title'>Bachelor of Science in Mathematics</h3>
                                     <h3>Aug 2010 - May 2014</h3>
                                 </div>
                                 <p>GPA: 3.8 | Magna Cum Laude</p>
                             </div>
                             <div className='resume-section__description'>
                                 <div className='resume-section__sub-title'>
-                                    <h3>Undergraduate Researcher</h3>
+                                    <h3 className='resume-section__education-title'>Undergraduate Researcher</h3>
                                     <h3>Oct 2012 - April 2013</h3>
                                 </div>
                                 <p>Ask me about the behavior of a given quadratic irrational's continued fraction expansion when multiplied by two.</p>
@@ -128,8 +124,8 @@ class Resume extends Component {
                     </div>
                 </div>
             </div>
-            
-           
+
+
         );
     }
 }
