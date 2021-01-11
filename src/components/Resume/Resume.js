f488a45a116beca194a59bf84da75fcfb6107483
Amanda Hussey <amanda.k.hussey@gmail.com>
2021-01-11 16:52:17 -0700

    bold

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -32,8 +32,8 @@ class Resume extends Component {
                         <div className='resume-section__description'>
                             <div className='resume-section__description__greatest-moment'>Greatest Contribution:</div>
                             <div className='resume-section__description__paragraph'>
-                                <p>Worked closely with designer to develop strategies around delivering best UI/UX practices.</p>
-                                <p>Onboarded junior hire. I truly enjoyed acting as a teacher/mentor!</p>
+                                <p>Worked closely with designer to develop strategies around delivering best <b>UI/UX</b> practices.</p>
+                                <p><b>Onboarded</b> junior hire. I truly enjoyed acting as a teacher/mentor!</p>
                             </div>
                         </div>
 
@@ -52,8 +52,8 @@ class Resume extends Component {
                         <div className='resume-section__description'>
                             <div className='resume-section__description__greatest-moment'>Proudest Moment:</div>
                             <div className='resume-section__description__paragraph'>
-                                <p>Led 10+ project scoping meetings after introducing an agile approach; received positive feedback through to executive level.</p>
-                                <p>I enjoyed being a go-between of sorts, helping bridge the gap between techincal and non-technical.</p>
+                                <p>Led 10+ project scoping meetings after introducing an <b>agile</b> approach; received positive feedback through to executive level.</p>
+                                <p>I enjoyed being a go-between of sorts, helping bridge the gap between <b>techincal and non-technical</b>.</p>
                             </div>
                         </div>
 
@@ -71,7 +71,7 @@ class Resume extends Component {
                              <div className='resume-section__description__greatest-moment'>Greatest Accomplishment:</div>
                              <div className='resume-section__description__paragraph'>
                                  <p>The Pack Station - A key feature of the web app. UI designed by me!</p>
-                                 <p>Executives loved it so much, they made sure it was shown in all major sales demos. It was even planned to be used in an advertising campaign.</p>
+                                 <p>Executives loved it so much, they made sure it was shown in all <b>major sales demos</b>. It was even planned to be used in an <b>advertising campaign</b>.</p>
                              </div>
                          </div>
                     </div>
@@ -88,7 +88,7 @@ class Resume extends Component {
                                 <div className='resume-section__sub-title'>
                                     <h3 className='resume-section__education-title'>Bachelor of Science in Mathematics</h3>
                                 </div>
-                                <p>GPA: 3.8 | Magna Cum Laude</p>
+                                <p><b>GPA: 3.8</b> | Magna Cum Laude</p>
                             </div>
                             <div className='resume-section__description'>
                                 <div className='resume-section__sub-title'>
